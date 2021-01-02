35c38d2fd3e1ff487599a80f85e326617ea5e603
Денис Юрченко <denis3101996@gmail.com>
2021-01-02 17:35:18 +0300

    ng For

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-toggle = false;
+arr = [1, 1, 2, 3, 5, 8, 13];
+  objs = [
+    {title: 'Post 1', author: 'Denis', comments: [
+        {name: 'Max', text: 'lorem 1'},
+        {name: 'Max', text: 'lorem 2'},
+        {name: 'Max', text: 'lorem 3'},
+      ]},
+    {title: 'Post 2', author: 'Denis 2', comments: [
+      {name: 'Max2', text: 'lorem 1'},
+      {name: 'Max2', text: 'lorem2'},
+      {name: 'Max2', text: 'lorem3'}
+  ]}
+  ];
 }
