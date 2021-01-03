26883e9529414bf2f122005a25deb774a7589b02
Денис Юрченко <denis3101996@gmail.com>
2021-01-03 17:03:35 +0300

    ViewEncapsulation (global style)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 export interface Post {
   title: string;
@@ -11,12 +11,25 @@ export interface Post {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   posts: Post[] = [
     {title: 'i want lear angular comp', text: 'i have learned com', id: 1},
     {title: 'next comp', text: 'lets go', id: 2}
   ];
+
+  ngOnInit(): void {
+  setTimeout(() => {
+    console.log('Timeout')
+    this.posts[0] = {
+      title: 'changed',
+      text: 'change2',
+      id: 33
+    }
+  }, 2000)
+  }
+
+
   updatePosts(post: Post) {
     this.posts.unshift(post);
     console.log('Post', post);
