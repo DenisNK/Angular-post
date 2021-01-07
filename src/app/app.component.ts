941a6c49d439fd12e28f16c531d19bf34f9b04e9
Денис Юрченко <denis3101996@gmail.com>
2021-01-07 11:21:00 +0300

    validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +11,23 @@ export class AppComponent implements OnInit{
   form: FormGroup;
 
   ngOnInit(): void {
-    this.form = new FormGroup({});
+    this.form = new FormGroup({
+      email: new FormControl('', [
+          Validators.email,
+          Validators.required
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6)
+      ])
+    });
   }
 
   submit() {
-  console.log('Form submited', this.form);
+    if (this.form.valid) {
+  console.log('Form=', this.form);
+  const formData = {...this.form.value};
+  console.log(formData);
+    }
   }
 }
