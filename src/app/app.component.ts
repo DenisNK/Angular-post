705899a204813c6c3539795a17ec301489735811
Денис Юрченко <denis3101996@gmail.com>
2021-01-17 18:42:05 +0300

    Create custom NgModels (swich On|Off)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,60 +1,14 @@
-import {Component, OnInit} from '@angular/core';
-import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
-import {MyValidators} from './my.validators';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
+  appState = 'off';
 
-  form: FormGroup;
-
-  ngOnInit(): void {
-    this.form = new FormGroup({
-
-        email: new FormControl('den@den.den', [
-          Validators.email,
-          Validators.required,
-          MyValidators.restrictedEmails
-        ], MyValidators.uniqEmail),
-        password: new FormControl(12345678, [
-          Validators.required,
-          Validators.minLength(5)
-      ]),
-        address: new FormGroup({
-          country: new FormControl('ua'),
-          city: new FormControl('', [Validators.required])
-      }),
-      skills: new FormArray([])
-    });
-  }
-
-  submit() {
-    if (this.form.valid) {
-  console.log('Form=', this.form);
-  const formData = {...this.form.value};
-  console.log(formData);
-  this.form.reset();
-    }
-  }
-
-  SetCapital() {
-    const cityMap = {
-      ru: 'Moscow',
-      ua: 'Kyiev',
-      by: 'Minsk'
-    };
-    const cityKey = this.form.get('address').get('country').value;
-    const city = cityMap[cityKey];
-    this.form.patchValue({address: {city}
-    });
-  }
-
-  addSkill() {
-    const control = new FormControl( '', Validators.required);
-    // (<FormArray> this.form.get('skills')).push();
-    (this.form.get('skills') as FormArray).push(control);
+  handleChange() {
+    console.log(this.appState);
   }
 }
