8d2f5e51cf65e19331dbdb3b7ad1aed1df490739
Денис Юрченко <denis3101996@gmail.com>
2021-01-19 20:18:35 +0300

    Routing module, active link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+
 import {AppComponent} from './app.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { SwichComponent } from './swich/swich.component';
+import {FormsModule} from '@angular/forms';
+import {AboutComponent} from './about/about.component';
+import {HomeComponent} from './home/home.component';
+import {PostsComponent} from './posts/posts.component';
+import {PostComponent} from './post/post.component';
+import {AboutExtraComponent} from './about-extra/about-extra.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SwichComponent,
+    AboutComponent,
+    HomeComponent,
+    PostsComponent,
+    PostComponent,
+    AboutExtraComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    AppRoutingModule
   ],
-  providers: [
-
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
