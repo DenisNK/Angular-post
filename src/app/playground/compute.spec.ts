7d68d3ec7eda28415a94c38e760aae01632a7285
Денис Юрченко <denis3101996@gmail.com>
2021-01-25 18:06:03 +0300

    Unit Tests

---
+++ src/app/playground/compute.spec.ts
@@ -0,0 +1,14 @@
+import {compute} from './compute';
+
+describe('compute', () => {
+
+  it('should return 0 if nagative input', () => {
+    const result = compute(-1);
+    expect(result).toBe(0);
+  });
+
+  it('should increment input if positive', () => {
+    const result = compute(5);
+    expect(result).toBe(6);
+  });
+});
