a54575b4d19de9506d005c172e98795f4cafbdb4
Денис Юрченко <denis3101996@gmail.com>
2021-01-27 15:28:54 +0300

    animation Keyframes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {animate, group, state, style, transition, trigger} from '@angular/animations';
+import {animate, group, keyframes, query, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -21,13 +21,23 @@ import {animate, group, state, style, transition, trigger} from '@angular/animat
       transition('end => start', animate('900ms ease-in-out')),
       /*transition('special <=> *', animate(500)), /!*переход из любого состояния в любое другое*!/*/
       transition('special <=> *', [
-        style({background: 'green'}),
-        animate(1000, style({background: 'pink'})),
-        animate(750)
+        group([query('h4', animate(1500, style({
+          fontSize: '.5rem',
+          color: 'red'
+        }))),
+          style({background: 'green'}),
+          animate(1000, style({background: 'pink'})),
+          animate(750)])
       ]), /*void => * */
       transition(':enter', [
-        style({opacity: 0 }),
-        animate('850ms ease-out')
+        animate('4s', keyframes([
+          style({background: 'red', offset: 0}),
+          style({background: 'black', offset: 0.2}),
+          style({background: 'orange', offset: 0.3}),
+          style({background: 'blue', offset: 1}),
+        ]))
+        /*style({opacity: 0 }),
+        animate('850ms ease-out')*/
       ]), /* * => void из любого (нулевого) в какое-то видимое*/
       transition(':leave', [
        style( {opacity: 1}),
