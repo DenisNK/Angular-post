c1a303b8d0743afa142435099fdebb6af7e62449
Денис Юрченко <denis3101996@gmail.com>
2021-01-27 16:58:32 +0300

    animation ng-Animate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,59 +1,11 @@
 import { Component } from '@angular/core';
-import {animate, group, keyframes, query, state, style, transition, trigger} from '@angular/animations';
+import {boxAnimation} from './app-animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [
-    trigger('box', [
-      state('start', style ({background: 'blue'})),
-      state('end', style({
-        background: 'red',
-        transform: 'scale(2.2)'
-      })),
-      state('special', style({
-        background: 'orange',
-        transform: 'scale(0.5)',
-        borderRadius: '50%',
-      })),
-      transition('start => end', animate(600)),
-      transition('end => start', animate('900ms ease-in-out')),
-      /*transition('special <=> *', animate(500)), /!*переход из любого состояния в любое другое*!/*/
-      transition('special <=> *', [
-        group([query('h4', animate(1500, style({
-          fontSize: '.5rem',
-          color: 'red'
-        }))),
-          style({background: 'green'}),
-          animate(1000, style({background: 'pink'})),
-          animate(750)])
-      ]), /*void => * */
-      transition(':enter', [
-        animate('4s', keyframes([
-          style({background: 'red', offset: 0}),
-          style({background: 'black', offset: 0.2}),
-          style({background: 'orange', offset: 0.3}),
-          style({background: 'blue', offset: 1}),
-        ]))
-        /*style({opacity: 0 }),
-        animate('850ms ease-out')*/
-      ]), /* * => void из любого (нулевого) в какое-то видимое*/
-      transition(':leave', [
-       style( {opacity: 1}),
-        group([
-          animate(750, style({
-          opacity: 0,
-          transform: 'scale(1.2)'
-        })),
-        animate(300, style( {
-          color: '#000',
-          fontWeight: 'bold'
-        }))
-       ])
-     ])
-    ])
-  ]
+  animations: [boxAnimation]
 })
 export class AppComponent {
   boxState = 'start';
@@ -61,4 +13,12 @@ export class AppComponent {
   animate(): void {
     this.boxState = this.boxState === 'end' ? 'start' : 'end';
   }
- }
+
+  animationStarted(event: AnimationEvent): void {
+    console.log('animationStarted', event);
+  }
+
+  animationDone(event: AnimationEvent): void {
+    console.log('animationDone', event);
+  }
+}
