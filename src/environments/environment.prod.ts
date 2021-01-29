330d0cb80c6693a5bf2970dd41694df506abe554
Денис Юрченко <denis3101996@gmail.com>
2021-01-29 14:27:17 +0300

    Added work with "FireBase" get auth token, login

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  apiKey: 'AIzaSyDLJ7zlYQtNHWi8iz3MlgtGU3-K1vFezi0',
   production: true
 };
