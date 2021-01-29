db166f09347d1404872ea78a2845ae08f832aa7f
Денис Юрченко <denis3101996@gmail.com>
2021-01-29 16:12:30 +0300

    Save token and lifecycle

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,20 +1,26 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {User} from '../../../shared/interfaces';
+import {FbAuthResponse, User} from '../../../shared/interfaces';
 import {Observable} from 'rxjs';
 import {environment} from '../../../../environments/environment';
 import {tap} from 'rxjs/operators';
-import {FbAuthRepos} from '../../../../environments/interface';
+
 
 @Injectable()
 export class AuthService {
 constructor(private http: HttpClient) {}
 
     get token(): string {
-     return '';
+    const expDate = new Date(localStorage.getItem('fb-token-exp'));
+    if (new Date() > expDate) {
+        this.logout();
+        return null;
+    }
+    return localStorage.getItem('fb-token');
     }
 
     login(user: User): Observable<any> {
+    user.returnSecureToken = true;
     return  this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
         .pipe(
             tap(this.setToken)
@@ -22,13 +28,21 @@ constructor(private http: HttpClient) {}
     }
 
     logout() {
+    this.setToken(null);
     }
 
     isAuthenticated(): boolean {
         return !!this.token;  /*!!Приводит к буллеан, если пустая строчка или null - false, если что-то есть true*/
     }
 
-    private setToken(response: FbAuthRepos) {
+    private setToken(response: FbAuthResponse | null) {
+        if (response) {
+            const expDate = new Date( new Date().getTime() + +response.expiresIn * 1000 );
+            localStorage.setItem('fb-token', response.idToken);
+            localStorage.setItem('fb-token-exp', expDate.toString()); /*localStorage содержит в себе только строки*/
+        } else {
+            localStorage.clear();
+        }
         console.log(response);
     }
 }
