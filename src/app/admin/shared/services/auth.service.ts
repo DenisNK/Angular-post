eea7de529e18bbd169734a5caefca6cc7f194557
Денис Юрченко <denis3101996@gmail.com>
2021-01-31 22:21:49 +0300

    Created Http Interceptor

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -6,7 +6,7 @@ import {environment} from '../../../../environments/environment';
 import {catchError, tap} from 'rxjs/operators';
 
 
-@Injectable()
+@Injectable({providedIn: 'root'})
 export class AuthService {
 
     public error$: Subject<string> = new Subject<string>(); /* Добавляем знак $ для стрима, Subject тот же Observal но еще и с событиями */
